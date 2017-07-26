70c83ae24afcdcc543bf6bf8d625bef3ceb0b983
Shahzaib Ather <shahzaibather7@github.ca>
2017-07-26 14:50:31 -0700

    New commit Wednesday

---
+++ Maths Game/QuestionManager.m
@@ -0,0 +1,13 @@
+//
+//  QuestionManager.m
+//  Maths Game
+//
+//  Created by Mohammad Shahzaib Ather on 2017-07-26.
+//  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
+//
+
+#import "QuestionManager.h"
+
+@implementation QuestionManager
+
+@end
